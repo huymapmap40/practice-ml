4f907c3f8841ee8810871a2ed94041db3a051efc
huymapmap40 <huymapmap40@gmail.com>
2020-08-20 23:48:23 +0700

    first commit

---
+++ download_data_housing.py
@@ -0,0 +1,21 @@
+import os
+import tarfile
+import urllib.request
+# import numpy as np
+import pandas as pd
+
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(HOUSING_PATH, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+  
+fetch_housing_data()
